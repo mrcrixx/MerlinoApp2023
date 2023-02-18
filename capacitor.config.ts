c367794de723f4fc0110c62358cd97cc121b1b7c
mrcrixx <mrcrixx@users.noreply.github.com>
2023-02-18 12:11:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MerlinoApp2023',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
